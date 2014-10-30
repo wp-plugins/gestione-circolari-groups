035d794689df34796a9d077ba8a8826783b38fb0
Ignazios <Ignazios@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-30 15:48:30 +0000

    pubblicazione versione 2.1
    git-svn-id: https://plugins.svn.wordpress.org/gestione-circolari-groups/tags/2.0.4@1016781 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/frontend.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * Gestione Circolari Groups
+ * 
+ * @package Gestione Circolari Groups
+ * @author Scimone Ignazio
+ * @copyright 2011-2014
+ * @since 2.0.4
+ */
+
+if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) { die('You are not allowed to call this page directly.'); }
+
+if (isset($_REQUEST['anno']))
+	$Anno=$_REQUEST['anno'];
+else
+	$Anno=date("Y");
+$Mese=0;
+if (isset($_REQUEST['mese']))
+	$Mese=$_REQUEST['mese'];
+$ret=circolariG_Lista_Circolari($Anno,$Mese);
+
+function circolariG_Lista_Circolari($Anno,$Mese){
+$Contenuto="";
+$IdCircolari=get_option('Circolari_Categoria');
+$mesecorrente = date('n');
+if (isset($_REQUEST['Anno']))
+	$annocorrente = $_REQUEST['Anno'];
+else
+	$annocorrente = date('Y');
+if (isset($_REQUEST['Mese']))
+	$mesecorrente=$_REQUEST['Mese'];
+elseif(isset($_REQUEST['Anno']))
+	$mesecorrente="";
+else
+	$mesecorrente=date('n');
+$args = array( 'category' => $IdCircolari,
+		       'post_type' => array('post','circolari'),
+			   'year' => $annocorrente,
+			   'monthnum' => $mesecorrente,
+			   'posts_per_page'  => -1,
+			   'post_status' => 'publish');
+$Circolari = get_posts($args);
+if (empty($Circolari)){
+	$Contenuto.='<h3>Non risultano circolari per '.$mesecorrente.' '.$annocorrente.' verranno visualizzate le ultime 5</h3>';
+	$args = array( 'category' => $IdCircolari,
+		       'post_type' => array('post','circolari'),
+			   'posts_per_page'  => 5,
+			   'post_status' => 'publish');
+	$Circolari = get_posts($args);
+}
+$Contenuto.=' <div>';
+//print_r($Circolari);
+$NumC=0;
+$accesso=new Groups_Post_Access();
+foreach($Circolari as $post) {
+	if($accesso->user_can_read_post($post->ID)){
+	//if (gcg_Is_Circolare_per_User($post->ID) or true){
+		$NumC++;
+			$Gruppi=gcg_get_Circolari_Gruppi();
+			$Destinatari=get_post_meta($post->ID, "_destinatari");
+			$Elenco="";
+			if(count($Destinatari)>0){
+				$Destinatari=unserialize($Destinatari[0]);
+				$Nomi_Des='';
+					foreach($Gruppi as $Gruppo)
+						if(array_search($Gruppo["Id"],$Destinatari)!==FALSE)
+							$Nomi_Des.=$Gruppo["Nome"].", ";
+				$Elenco=substr($Nomi_Des,0,strlen($Nomi_Des)-2);
+			}
+		$Contenuto.='
+		<div style="margin-bottom:15px;padding:3px;">';
+		$numero=get_post_meta($post->ID, "_numero",TRUE);
+		$anno=get_post_meta($post->ID, "_anno",TRUE);
+		$Contenuto.='
+			<h4><a href="'.get_permalink($post->ID).'">'.$post->post_title.'</a></h4>
+			<div style="font-weight: bold;font-size:0.8em;margin-top:3px;">Del '.gcg_FormatDataItaliano($post->post_date).' Numero '.$numero.'_'.$anno.'</div> 
+			<div style="height:30px;">
+				<div style="display:inline;">
+					<img src="'.Circolari_URL.'/img/tipo.png" style="border:0;float:left;" alt="Icona tipo circolare"/>
+				</div>
+				<div style="display:inline;vertical-align:top;">
+					<p style="font-style:italic;font-weight:bold;font-size:0.9em;display:inline;margin-top:3px;">'.$post->post_type.'</p>
+				</div>';
+		if ($post->post_type=="circolari")
+			$Contenuto.='
+				<div style="display:inline;">
+					<img src="'.Circolari_URL.'/img/destinatari.png" style="border:0;" alt="Icona destinatari"/>
+				</div>
+				<div style="display:inline;vertical-align:top;">
+					<p style="font-style:italic;font-weight:bold;font-size:0.9em;display:inline;margin-top:3px;">'.$Elenco.'</p>
+				</div>';
+		if (!post_password_required( $post->ID ))
+			$riassunto=	$post->post_excerpt;
+		else{
+			$riassunto="";
+		}
+		if (!empty($post->post_password))
+			$Contenuto.='
+				<div style="display:inline;">
+					<img src="'.Circolari_URL.'img/protetto.png" style="border:0;" alt="Icona protezione"/>
+				</div>
+				<div style="display:inline;vertical-align:top;">
+					<p style="font-style:italic;font-size:0.8em;display:inline;margin-top:3px;">Contenuto Protetto</p>
+				</div>';	
+		if (gcg_Is_Circolare_Da_Firmare($post->ID))
+			if (!gcg_Is_Circolare_Firmata($post->ID)) {
+				$ngiorni=gcg_GetscadenzaCircolare($post->ID,"",True);		
+				if(gcg_Is_Circolare_Scaduta($post->ID)){
+					$Contenuto.='
+					<div style="display:inline;">
+						<img src="'.Circolari_URL.'/img/firma.png" style="border:0;" alt="Icona firma o presa visione"/>
+					</div>
+					<div style="display:inline;vertical-align:top;">
+						<p style="font-style:italic;font-size:0.8em;display:inline;margin-top:3px;color:red;">Scaduta da '.abs($ngiorni).' giorni</p>
+					</div>';
+				}else{
+					switch ($ngiorni){
+						case -1:							
+							$entro="";							
+							break;													
+						case 0:
+							$entro="entro OGGI";
+							break;
+						case 1:
+							$entro="entro DOMANI";
+							break;
+						default:
+							$entro="entro $ngiorni giorni";
+							break;
+					}
+					if (get_post_meta($post->ID, "_sciopero",TRUE)=="Si")
+						$Tipo="Esprimere adesione $entro";
+					else
+						if (get_post_meta($post->ID, "_firma",TRUE)=="Si")
+							$Tipo="Firmare $entro";
+					$Contenuto.='
+					<div style="display:inline;">
+						<img src="'.Circolari_URL.'/img/firma.png" style="border:0;" alt="Icona firma o presa visione"/>
+					</div>
+					<div style="display:inline;vertical-align:top;">
+						<p style="font-style:italic;font-size:0.8em;display:inline;margin-top:3px;color:red;">'.$Tipo.'</p>
+					</div>';	
+				}
+		}else{
+			$Contenuto.='
+			<div style="display:inline;">
+				<img src="'.Circolari_URL.'/img/firma.png" style="border:0;" alt="Icona firma o presa visione"/>
+			</div>
+			<div style="display:inline;vertical-align:top;">
+				<p style="font-style:italic;font-size:0.8em;display:inline;margin-top:3px;color:blue;">Firmata</p>
+			</div>';				
+		}
+		$Contenuto.='	
+			</div>
+			<div style="margin-bottom:5px;">
+				<em>'.$riassunto .'</em>
+			</div>
+			<hr />
+		</div>';
+	}
+}
+$Contenuto.= '
+		</div>';
+if ($NumC==0)
+	$Contenuto.='<span style="color:red;font-style: italic;">Nessuna Circolare filtrata</span>';
+$Contenuto.='	<div style="clear:both"></div>';
+return $Contenuto;
+}
+?>
